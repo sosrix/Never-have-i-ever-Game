ad216a28e9251e8e804c413bf74aad77e9e346a7
SOUFIYAN AHMINA <ahminasoufiyan@gmail.com>
2022-06-05 15:41:15 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import "./App.css";
 
-
 function App() {
   return (
-
     <div className="App">
-
-    
+      <h1>Never Have I Ever Game</h1>
+      <button>Next Question</button>
+      <div>
+        <h2>Scoreboard</h2>
+        <div>
+          <button> Player 1 | +I HAVE : 0</button>
+        </div>
+        <h3>
+          <button> Player 2 | +I HAVE : 0</button>
+        </h3>{" "}
+        <h3>
+          <button> Player 3 | +I HAVE : 0</button>
+        </h3>
       </div>
-
+    </div>
   );
 }
+
+export default App;
+
