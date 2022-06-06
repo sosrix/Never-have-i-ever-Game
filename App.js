257dd719e4741a5fef41fa157dd19ab14981b33c
SOUFIYAN AHMINA <ahminasoufiyan@gmail.com>
2022-06-06 11:18:08 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import PlayerScore from "./PlayerScore";
 import data from "./Questions.js";
 
 function App() {
   const [questionId, setQuestionId] = useState(0);
-  const [score, setScore] = useState(0);
   const [idUsed, setIdUsed] = useState([]);
+
   function randomNumberGen() {
     return Math.floor(Math.random() * data.questions.length);
   }
@@ -21,9 +22,7 @@ function App() {
       setIdUsed(oldUsed);
     }
   }
-  function addPoint() {
-    setScore((preValue) => ++preValue);
-  }
+
   return (
     <div className="App">
       <h1>Never Have I Ever Game</h1>
@@ -31,10 +30,10 @@ function App() {
       <h3> {data.questions[questionId]}</h3>
       <div>
         <h2>Scoreboard</h2>
-
-        <button onClick={addPoint}>
-          <input placeholder="player's Name" /> | +I HAVE : {score}
-        </button>
+        <PlayerScore />
+        <PlayerScore />
+        <PlayerScore />
+        <PlayerScore />
       </div>
     </div>
   );
